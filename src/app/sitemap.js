c7d493eefe0dfca7b71cba22e6593585fc20a1f5
Sanan <senan996@gmail.com>
2025-11-16 15:04:05 +0100

    api route to sitemap & robots

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -29,7 +29,7 @@ export default async function sitemap() {
   routing.locales.forEach((locale) => {
     // Static routes
     staticRoutes.forEach((route) => {
-      const path = route ? `/${locale}/${route}` : `/${locale}`;
+      const path = route ? `${locale}/${route}` : `/${locale}`;
 
       urls.push({
         url: `${siteUrl}${path}`,
@@ -40,7 +40,7 @@ export default async function sitemap() {
     // Dynamic expertise routes
     expertisePaths.forEach((path) => {
       urls.push({
-        url: `${siteUrl}/${locale}${path}`,
+        url: `${siteUrl}${locale}${path}`,
         lastModified: new Date(),
       });
     });
